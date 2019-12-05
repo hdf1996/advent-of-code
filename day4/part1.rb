5efe00a999fda589f5bf3872d22ed2aa732ecf7d
hdf1986 <hugo.farji+github@gmail.com>
2019-12-05 08:39:02 -0300

    Added day 4

---
+++ day4/part1.rb
@@ -0,0 +1,11 @@
+range = 134792..675810
+
+def check_password(password)
+  digits = password.to_s.split('')
+  digits.length == 6 &&
+  digits.chunk(&:itself).map{|_, v| v.length}.any? { |e| e >= 2 } &&
+  digits.sort == digits
+end
+
+count = range.count { |password| check_password(password) }
+puts count
