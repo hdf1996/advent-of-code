9f1c5d35f1784b09441ddb37cd62dabcd1262c52
Hugo David Farji <hugo.farji@gmail.com>
2020-12-02 10:41:17 -0300

    Added xor operator to day 2 2020

--- 2020/day2/ruby/part2.rb
+++ 2020/day2/ruby/part2.rb
@@ -5,10 +5,8 @@ valid_count = input.count do |line|
 	rule_positions, rule_letter = rule.split(' ')
 	rule_first_position, rule_second_position = rule_positions.split('-').map(&:to_i)
 
-	(password[rule_first_position - 1] == rule_letter &&
-		password[rule_second_position - 1] != rule_letter) || 
-	(password[rule_first_position - 1] != rule_letter &&
-	 	password[rule_second_position - 1] == rule_letter)
+	(password[rule_first_position - 1] == rule_letter) ^
+		(password[rule_second_position - 1] == rule_letter) 
 end
 
 puts valid_count
